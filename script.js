e365ba744f05de1534b8cb6453e75db727fbb333
DinaTaklit <ez_taklit@esi.dz>
2021-03-16 23:13:11 +0100

    Setup Calculator Values

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded',init);
+const opts = ['*', '/', '+', '-', '9', '8', '7', '6', '5', '4', '3', '2', '1', '0', '.']; //all keys
+const spec = ['*', '/', '+', '-']; //special function keys
+
+function init(){
+
+}
