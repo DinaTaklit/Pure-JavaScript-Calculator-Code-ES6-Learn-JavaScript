876a60038e0ac04968f685baab9d659188dd49da
DinaTaklit <ez_taklit@esi.dz>
2021-03-17 10:41:14 +0100

    comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// This program does not fix error of type 4 ++ 3
 
 // Once document is ready 
 window.addEventListener('DOMContentLoaded',init);
@@ -6,7 +7,7 @@ window.addEventListener('DOMContentLoaded',init);
 const opts = ['*', '/', '+', '-', '9', '8', '7', '6', '5', '4', '3', '2', '1', '0', '.']; //all keys
 const spec = ['*', '/', '+', '-']; //special function keys
 
-// Create DOM page Elements
+
 function init(){
   document.title = "JavaScript ES6 Calculator";
 
@@ -14,53 +15,59 @@ function init(){
   let dec = false; // for decimal value
   let eva = false; // for evaluation
 
+  // Create DOM page Elements
   const container = document.createElement('div');
   container.classList.add('container');
   container.style.maxWidth = "600px";
   container.style.margin = "auto";
   document.body.appendChild(container);
-  const output = document.createElement('input');
-  output.setAttribute('type','text');
-  output.classList.add('output');
-  output.style.width = '100%';
-  output.style.lineHeight = "50px";
-  output.style.fontSize = "3rem";
-  output.style.textAlign = "right";
-  container.appendChild(output);
+  const input = document.createElement('input');
+  input.setAttribute('type','text');
+  input.classList.add('input');
+  input.style.width = '100%';
+  input.style.lineHeight = "50px";
+  input.style.fontSize = "3rem";
+  input.style.textAlign = "right";
+  container.appendChild(input);
   const main = document.createElement('div');
   main.classList.add('main');
   main.style.width = "100%";
   container.appendChild(main);
+
+  // Create keys with theire event listner
   opts.map( val => {
-    btnMaker(val, addOutput);
+    btnMaker(val, addinput);
   });
+
   // add equal to evaluate the expression and clear button to clear up content
-  btnMaker("=", evalOutput);
-  btnMaker("C", clrOutput);
+  btnMaker("=", evalinput);
+  btnMaker("C", clrinput);
 
   // Function color up the border of the text input in case of error (red color) or nrml case (black color)
-  function cOutput(v) {
-    output.style.border = v + ' 1px solid';
-    output.style.color = v;
+  function cinput(v) {
+    input.style.border = v + ' 1px solid';
+    input.style.color = v;
   }
  
-  function evalOutput(){
-    cOutput('black');
-    console.log("eva =" + eva);
-    if (output.value === "" || eva){
-       cOutput('red');
+  // Function to evaluate the input
+  function evalinput(){
+    cinput('black'); // back to black border in every nrml state
+    if (input.value === "" || eva){ // if the input is empty or we have an operator in the end  like 3 + 3 +
+       cinput('red');
     }
     else {
-      output.value = eval(output.value);
+      input.value = eval(input.value);
     }
-    dec = output.value.includes('.'); // update the decimal boolean if the output cotain "."
+    dec = input.value.includes('.'); // update the decimal boolean if the input cotain "."
   }
 
-  function clrOutput(){
-    cOutput('black');
-    output.value = "";
+  // Function to color up the input border
+  function clrinput(){
+    cinput('black');
+    input.value = "";
   }
 
+  // Function that create keys
   function btnMaker(txt, myFunction){
       let btn = document.createElement('button');
       btn.setAttribute('type', 'button');
@@ -70,26 +77,28 @@ function init(){
       btn.style.fontSize = '2em';
       btn.val = txt;
       btn.textContent = txt;
-      btn.addEventListener('click', myFunction);
+      btn.addEventListener('click', myFunction); // add an event listner to the key
       main.appendChild(btn);
   }
-  function addOutput(e){
-     cOutput('black');
+
+  // Function update the input with key value after each click
+  function addinput(e){
+    cinput('black');
     let char = e.target.val;
     if (char == "."){ // check if we have multiple "." in the expression
       if (dec){
         char = '';
-        cOutput('red');
+        cinput('red');
       }
       else { // update dec with true once we face a "."
         dec = true;
       }
     }
     eva = spec.includes(char); // check if the char is included in the spec "['*', '/', '+', '-']"
-    console.log ("eva in add output = " + eva);
+    console.log ("eva in add input = " + eva);
     if (eva){
       dec = false; // return dec to false to be able to write other numbers with "."
     }
-    output.value += char;
+    input.value += char;
   }
 }
